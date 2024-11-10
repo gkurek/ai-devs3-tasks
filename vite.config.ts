260f7678cd514630d1b40d2251ce0dea40e2cba8
gkurek <grzegorz.tomasz.kurek@gmail.com>
2024-11-10 18:19:58 +0100

    add proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://xyz.ag3nts.org",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+console.log("VITE_AGENTS_URL", process.env.VITE_AGENTS_URL);
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [vue()],
+    server: {
+      proxy: {
+        "/api/centrala": {
+          target: env.VITE_CENTRALA_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api\/centrala/, ""),
+        },
+        "/api": {
+          target: env.VITE_AGENTS_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
+  };
 });
